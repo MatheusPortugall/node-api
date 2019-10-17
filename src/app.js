9255ecf60bfe173b9b16c06e850dc3652af32587
Matheus Portugal <matheusportugal53@gmail.com>
2019-10-17 08:37:33 -0300

    Create order route

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ const Order = require("./models/order");
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
 const customerRoute = require('./routes/customer-route');
+const orderRoute = require('./routes/order-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,5 +25,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', indexRoute);
 app.use('/products', productRoute);
 app.use('/customers', customerRoute);
+app.use('/order', orderRoute);
 
 module.exports = app;
