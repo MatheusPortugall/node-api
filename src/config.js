64e262032ef0382ff0aeb60cba011918f9e6f838
Matheus Portugal <matheusportugal53@gmail.com>
2019-10-30 11:06:13 -0300

    Setting up email sending

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao Node Store.';
 
 module.exports = {
     connectionString: 'mongodb://localhost:27017/nodestr',
-    sendgridKey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD',
     key: global.SALT_KEY,
+    email_tmpl: '<strong>{0}</strong>'
 };
--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -3,6 +3,8 @@ const repository = require("../repositories/customer-repository");
 const md5 = require("md5");
 const config = require("../config");
 
+const emailService = require("../services/email-service");
+
 exports.post = async (req, res, next) => {
     let contract = new ValidationContract;
     contract.hasMinLen(req.body.name, 3, "O nome deve conter pelo menos 3 caracteres");
@@ -20,6 +22,9 @@ exports.post = async (req, res, next) => {
             email: req.body.email,
             password: md5(req.body.password + config.key)
         });
+
+        emailService.send(req.body.email, 'Bem vindo ao Node Store', config.email_tmpl.replace('{0}', req.body.name ));
+
         res.status(201).send({success: true, message: "Cliente cadastrado com sucesso!"});
     } catch (e) {
         res.status(500).send({success: false, message: "Falha ao processar sua requisição", data: e});
